7b511a4ffc53b9248deee4f9bc699044dd03787f
Drew Ross <42723693+AndrewGallowitch@users.noreply.github.com>
2018-09-21 21:03:23 -0400

    final commit

--- java.js
+++ java.js
@@ -1,10 +1,11 @@
 
 // array of strings to start with.
-var actors = ["Kevin Hart", "Will Ferrel", "Will Smith", "Mark Wahlberg"];
+var actors = ["Kevin Hart", "Will Ferrel", "Will Smith", "Seth Rogan", "Sandra Bullock", "Denzel Washington"];
 
 
 //  ajax function after onlick of buttons.
 function displayGiphy() {
+    $(".result-container").empty()
     var person = $(this).attr("data-name");
 
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + person + "&api_key=dc6zaTOxFJmzC&limit=10";
@@ -17,16 +18,24 @@ function displayGiphy() {
         var results = response.data;
 
         for (var i = 0; i < results.length; i++) {
-            var gifDiv = $("<div>");
+            var gifDiv = $("<div class = 'col-6'>");
 
             var rating = results[i].rating;
 
-            var p = $("<p>").text("Rating: " + rating);
+            var para = $("<p>").text("Rating: " + rating);
 
+            var stillImage = results[i].images.fixed_height_still.url;
+            var movingImage = results[i].images.fixed_height.url;
+            
+            
             var personImage = $("<img>");
-            personImage.attr("src", results[i].images.fixed_height_still.url);
+            personImage.attr("src", stillImage);
+            personImage.attr("still", stillImage);
+            personImage.attr("moving", movingImage);
+            personImage.addClass("gifTacular");
 
-            gifDiv.prepend(p);
+
+            gifDiv.prepend(para);
             gifDiv.prepend(personImage);
 
             $(".result-container").prepend(gifDiv);
@@ -61,6 +70,22 @@ $("#add-gif").on("click", function (event) {
     renderButtons();
 });
 
+$(document).on("click", ".gifTacular", function(){
+    // compare src to still and moving images
+    // use an if statement
+    
+    var srcattr = $(this).attr("src")
+    var stillattr = $(this).attr("still")
+    var movingattr = $(this).attr("moving")
+    
+    // if src equals still then on click switch to moving
+    if (srcattr === stillattr){
+        $(this).attr("src", movingattr)
+    } else if (srcattr === movingattr){
+        $(this).attr("src", stillattr)
+    }
+    
+});
 
 $(document).on("click", ".giphy-btn", displayGiphy);
 renderButtons();
